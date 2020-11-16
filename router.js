c151798428d6319ecf28cf114254a584f697ce6a
J.S <jimmy.sene@laposte.net>
2020-11-16 20:00:14 +0100

    Liste des promos

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const promos = require("./data/promos.json");
 
 router.get("/", (req, res) => {
   res.render("accueil");
+});
+
+router.get("/promos", (req, res) => {
+  res.render("promos", { promos });
 });
 
 module.exports = router;
