9d91bd96db87e170110b82df41107e49465ea686
J.S <jimmy.sene@laposte.net>
2020-11-16 19:45:24 +0100

    Création du serveur et page d'accueil

---
+++ index.js
@@ -0,0 +1,13 @@
+// Créer le serveur web
+const express = require("express");
+const app = express();
+const router = require("./router");
+
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.use(express.static("/public"));
+
+app.use(router);
+
+app.listen(process.env.PORT || 3000);
